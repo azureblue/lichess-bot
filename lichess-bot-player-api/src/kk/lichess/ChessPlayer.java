7f7fbb7e13366d86491698128d8d18d6aa103183
azureblue <kk0xf0@gmail.com>
2019-11-15 01:41:46 +0100

    removing api module

--- lichess-bot-player-api/src/kk/lichess/ChessPlayer.java
+++
@@ -1,9 +0,0 @@
-package kk.lichess;
-
-public interface ChessPlayer {
-    void gameStarts(String initialFen, boolean playerIsWhite, int remainingTime);
-
-    String makeMove(int remainingTime);
-
-    void applyMove(String move);
-}
